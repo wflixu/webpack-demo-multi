dcd16190b85f4b098b74795d3797abaa63bb5028
wflixu <wflixu@yeah.net>
2018-08-12 19:38:06 +0800

    test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,3 @@
-import _ from 'lodash';
-function component() {
-    var element = document.createElement('div');
-  
-    // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
-
+const show = require('./show.js');
+// 执行 show 函数
+show('Webpack');
