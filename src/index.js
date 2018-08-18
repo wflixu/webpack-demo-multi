269e17527f999b6592fa1592c27c9f5f0d503d8b
wflixu <wflixu@yeah.net>
2018-08-18 12:27:41 +0800

    alter

--- src/index.js
+++ src/index.js
@@ -1,25 +1,18 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
-import Data from './data.xml';
+
 import printMe from './print.js';
 function component() {
     var element = document.createElement('div');
   
     // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-    var myIcon = new Image();
-   myIcon.src = Icon;
+
    var btn = document.createElement('button');
    
    btn.innerHTML = 'Click me and check the console!';
    btn.onclick = printMe;
    element.appendChild(btn);
-   element.appendChild(myIcon);
 
-   console.log(Data);
-  
     return element;
   }
   
--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ module.exports = {
     plugins: [
         new CleanWebpackPlugin(['dist']),
         new HtmlWebpackPlugin({
+            template:'./index.html',
             title: 'Output Management'
         })
     ],
